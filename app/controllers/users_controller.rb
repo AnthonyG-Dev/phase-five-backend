f401b3dd59fb2e46bb08926a2b77746ac18dc639
GicheruAnthony <giterugicheru@gmail.com>
2023-07-28 08:28:09 +0300

    mainly controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,55 @@
 class UsersController < ApplicationController
+  # Action to show a list of all users
+  def index
+    @users = User.all
+  end
+
+  # Action to display a specific user's details
+  def show
+    @user = User.find(params[:id])
+  end
+
+  # Action to render a form for creating a new user
+  def new
+    @user = User.new
+  end
+
+  # Action to handle the creation of a new user
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user, notice: 'User was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # Action to render a form for editing a user's details
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  # Action to handle updating a user's details
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user, notice: 'User was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # Action to handle deleting a user
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_url, notice: 'User was successfully deleted.'
+  end
+
+  private
+
+  # Strong parameters to whitelist user attributes for security
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# seeds.rb
+puts 'seeding .....'
+# Create Courses
+courses = Course.create([
+  { name: 'Course 1', start: Date.today, end: Date.today + 1.week, description: 'Description for Course 1' },
+  { name: 'Course 2', start: Date.today, end: Date.today + 2.weeks, description: 'Description for Course 2' }
+])
+
+# Create Users
+users = User.create([
+  { name: 'User 1', password: 'password1', role: 'role1', avatar: 'avatar1', course: courses.first },
+  { name: 'User 2', password: 'password2', role: 'role2', avatar: 'avatar2', course: courses.last }
+])
+
+# Create Announcements
+announcements = Announcement.create([
+  { content: 'Announcement 1 for Course 1', course: courses.first },
+  { content: 'Announcement 2 for Course 2', course: courses.last }
+])
+
+# Create Sessions
+sessions = Session.create([
+  { start: Date.today, end: Date.today + 1.day, link: 'session_link_1', course: courses.first },
+  { start: Date.today, end: Date.today + 2.days, link: 'session_link_2', course: courses.last }
+])
+
+# Create Comments
+comments = Comment.create([
+  { content: 'Comment 1 for Session 1', session: sessions.first, user: users.first },
+  { content: 'Comment 2 for Session 2', session: sessions.last, user: users.last }
+])
+
+puts 'done !!!!!!!!!'
