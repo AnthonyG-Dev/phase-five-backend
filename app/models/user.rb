b2f77a82eb5222dfe5e5633d1c5f2aebed093a7b
GicheruAnthony <giterugicheru@gmail.com>
2023-08-07 12:45:04 +0300

    email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   belongs_to :course
   has_many :comments
   has_secure_password
+  
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
 end
